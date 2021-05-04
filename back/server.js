952134a3ae2b8e0fbf666b10e0365b6b7c3e4792
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-04 14:44:30 +0200

    dev: static server

--- back/server.js
+++ back/server.js
@@ -3,9 +3,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res, next) => {
-  res.send("Hello World!");
+app.use((req, res, next) => {
+  console.log("req.url: ", req.url);
+  next();
 });
+
+app.use(express.static("."));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
