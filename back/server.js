ee74c022cd08630d729c6f6720bb6a75aa1aa3a8
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-04 14:25:22 +0200

    dev: implemented first server

--- back/server.js
+++ back/server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res, next) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
