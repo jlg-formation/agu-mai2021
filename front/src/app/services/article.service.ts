7b1f1923ef37b3a881e32dfa63a485c119d6c789
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-06 11:55:52 +0200

    dev: localstorage for more persistence.

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,32 +5,49 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles = [
-    {
-      name: 'Tournevis',
-      price: 2.99,
-      qty: 123,
-    },
-    {
-      name: 'Tournevis cruciforme',
-      price: 3.45,
-      qty: 12,
-    },
-    {
-      name: 'Tondeuse à gazon',
-      price: 150,
-      qty: 20,
-    },
-    {
-      name: 'Marteau',
-      price: 3.67,
-      qty: 300,
-    },
-  ];
+  articles: Article[] = [];
 
-  constructor() {}
+  constructor() {
+    this.load();
+  }
+
+  save(): void {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
+  }
+
+  load(): void {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      this.articles = [
+        {
+          name: 'Tournevis',
+          price: 2.99,
+          qty: 123,
+        },
+        {
+          name: 'Tournevis cruciforme',
+          price: 3.45,
+          qty: 12,
+        },
+        {
+          name: 'Tondeuse à gazon',
+          price: 150,
+          qty: 20,
+        },
+        {
+          name: 'Marteau',
+          price: 3.67,
+          qty: 300,
+        },
+      ];
+      return;
+    }
+    const articles = JSON.parse(str);
+    this.articles = articles as Article[];
+  }
 
   add(article: Article): void {
     this.articles.push(article);
+    this.save();
   }
 }
