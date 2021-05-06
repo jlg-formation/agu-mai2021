386f37f8e4cb0ad57dda5f52d0bf83877ca00cd2
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-06 10:56:49 +0200

    dev: loop *ngFor and expression

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles = [
+    {
+      name: 'Tournevis',
+      price: 2.99,
+      qty: 123,
+    },
+    {
+      name: 'Tournevis cruciforme',
+      price: 3.45,
+      qty: 12,
+    },
+    {
+      name: 'Tondeuse à gazon',
+      price: 150,
+      qty: 20,
+    },
+    {
+      name: 'Marteau',
+      price: 3.67,
+      qty: 300,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
