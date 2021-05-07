5dd5331200841f0b7b69a21f5f96be70cdbb6042
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-07 10:09:04 +0200

    test: added an article

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -3,6 +3,7 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { article } from 'src/test/data';
 
 import { HttpArticleService } from './http-article.service';
 
@@ -34,6 +35,23 @@ describe('HttpArticleService', () => {
     const req = http.expectOne('/api/articles');
     expect(req.request.method).toEqual('GET');
     req.flush('', { status: 404, statusText: 'Not Found' });
+    expect(service).toBeTruthy();
+  });
+
+  it('should add an article', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+
+    service.add(article);
+
+    const req2 = http.expectOne('/api/articles');
+    expect(req2.request.method).toEqual('POST');
+    req2.flush('');
+
+    const req3 = http.expectOne('/api/articles');
+    expect(req3.request.method).toEqual('GET');
+    req3.flush([article]);
     expect(service).toBeTruthy();
   });
 });
---
+++ front/src/test/data.ts
@@ -0,0 +1,8 @@
+import { Article } from 'src/app/interfaces/article';
+
+export const article: Article = {
+  id: 'a23',
+  name: 'test-data',
+  price: 1.22,
+  qty: 3,
+};
