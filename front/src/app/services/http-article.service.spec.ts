224dd0ee53f9b41990feebdfe1e118c89ca85eea
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-07 10:23:24 +0200

    test: http article coverage

--- front/src/app/services/http-article.service.spec.ts
+++ front/src/app/services/http-article.service.spec.ts
@@ -4,6 +4,7 @@ import {
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { article } from 'src/test/data';
+import { Article } from '../interfaces/article';
 
 import { HttpArticleService } from './http-article.service';
 
@@ -52,6 +53,54 @@ describe('HttpArticleService', () => {
     const req3 = http.expectOne('/api/articles');
     expect(req3.request.method).toEqual('GET');
     req3.flush([article]);
+    expect(service).toBeTruthy();
+  });
+
+  it('should add an article with error', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+
+    service.add(article);
+
+    const req2 = http.expectOne('/api/articles');
+    expect(req2.request.method).toEqual('POST');
+    req2.flush('', { status: 500, statusText: 'Internal Error' });
+
+    expect(service).toBeTruthy();
+  });
+
+  it('should remove an article with error', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+
+    const set = new Set<Article>();
+    set.add(article);
+    service.remove(set);
+
+    const req2 = http.expectOne('/api/articles');
+    expect(req2.request.method).toEqual('DELETE');
+    req2.flush('', { status: 500, statusText: 'Internal Error' });
+
+    expect(service).toBeTruthy();
+  });
+
+  it('should remove an article', () => {
+    const req = http.expectOne('/api/articles');
+    expect(req.request.method).toEqual('GET');
+    req.flush([]);
+
+    service.remove(new Set());
+
+    const req2 = http.expectOne('/api/articles');
+    expect(req2.request.method).toEqual('DELETE');
+    req2.flush('', { status: 204, statusText: 'No Content' });
+
+    const req3 = http.expectOne('/api/articles');
+    expect(req3.request.method).toEqual('GET');
+    req3.flush([article]);
+
     expect(service).toBeTruthy();
   });
 });
