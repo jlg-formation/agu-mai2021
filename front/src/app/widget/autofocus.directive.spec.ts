c1fdc1b81adf7c0c93b2a6f92cf2770604a2ae19
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-05-07 11:17:23 +0200

    test: directive fully tested.

--- front/src/app/widget/autofocus.directive.spec.ts
+++ front/src/app/widget/autofocus.directive.spec.ts
@@ -1,8 +1,41 @@
+import { Component } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AutofocusDirective } from './autofocus.directive';
 
+@Component({
+  selector: 'app-test',
+  template: '<div><input type="text" appAutofocus [delay]="delay"></div>',
+})
+export class TestComponent {
+  delay = 500;
+}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AutofocusDirective(undefined);
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TestComponent, AutofocusDirective],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    const compiled = fixture.nativeElement;
+    const input = compiled.querySelector('input');
+    const focusElt = document.activeElement;
+    expect(input).toEqual(focusElt);
+  });
+
+  it('should change input', () => {
+    component.delay = 1000;
+    fixture.detectChanges();
+    expect(true).toBeTrue();
   });
 });
